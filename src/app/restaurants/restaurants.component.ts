47263e953499273e9d6ea2823fa83f06a4c48aeb
Willian <willianfalbo@gmail.com>
2019-05-08 02:25:49 -0300

    implemented search bar on restaurants page

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,21 +1,70 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+
+import { Observable } from 'rxjs/Observable'
+import 'rxjs/add/operator/switchMap'
+import 'rxjs/add/operator/do'
+import 'rxjs/add/operator/debounceTime'
+import 'rxjs/add/operator/distinctUntilChanged'
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/observable/from'
 
 import { Restaurant } from './restaurant/restaurant.model';
 import { RestaurantsService } from './restaurant.service';
 
 @Component({
   selector: 'mt-restaurants',
-  templateUrl: './restaurants.component.html'
+  templateUrl: './restaurants.component.html',
+  animations: [
+    trigger('toggleSearch', [
+      state('hidden', style({
+        opacity: 0,
+        'max-height': '0px'
+      })),
+      state('visible', style({
+        opacity: 1,
+        'max-height': '70px',
+        'margin-top': '20px'
+      })),
+      transition('* => *', animate('300ms 0s ease-in-out'))
+    ])
+  ]
 })
 export class RestaurantsComponent implements OnInit {
 
+  searchBarState: string = 'hidden'
   restaurants: Restaurant[]
 
-  constructor(private restaurantsService: RestaurantsService) { }
+  searchForm: FormGroup
+  searchControl: FormControl
+
+  constructor(private restaurantsService: RestaurantsService, private fb: FormBuilder) { }
 
   ngOnInit() {
+
+    this.searchControl = this.fb.control('')
+    this.searchForm = this.fb.group({
+      searchControl: this.searchControl
+    })
+
+    //create an event for value changes
+    this.searchControl.valueChanges
+      .debounceTime(500) //delays the values emitted within this time. If a new value arrives, the previous pending value is dropped.
+      .distinctUntilChanged() //ignore equal searchs after debounceTime
+      // .do(searchTerm => console.log(`q=${searchTerm}`))
+      .switchMap(searchTerm =>
+        this.restaurantsService
+          .restaurants(searchTerm)
+          .catch(error => Observable.from([])))
+      .subscribe(resp => this.restaurants = resp)
+
     this.restaurantsService.restaurants()
       .subscribe(resp => this.restaurants = resp)
+  }
+
+  toogleSearch() {
+    this.searchBarState = this.searchBarState == 'hidden' ? 'visible' : 'hidden'
   }
 
 }
